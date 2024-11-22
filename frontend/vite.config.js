1d972d85d07bdf3b1432819c96cd5964a411ec73
Chand Khan <envy03247@gmail.com>
2024-11-22 16:57:47 +0530

    mine

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,10 +9,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    host: '0.0.0.0',  // Allow access from any device on the local network
-    port: 4000,        // Change the port number to 4000
-    strictPort: true,  // If true, Vite will exit if the port is already in use
-    open: true,        // Automatically open the browser when the server starts
-  },
 })
